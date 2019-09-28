7ca800729bf5af0fd5a04f9d23154766b5d666e6
Xwilarg <xwilarg@yahoo.fr>
2019-09-28 22:29:14 +0900

    Add player rotation

---
+++ Assets/Scripts/HeadController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class HeadController : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        transform.parent.GetComponent<SkinnedMeshRenderer>().enabled = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+                Cursor.lockState = CursorLockMode.None;
+            else
+                Cursor.lockState = CursorLockMode.Locked;
+        }
+        transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * -1f, 0f, 0f));
+        transform.Rotate(new Vector3(0f, Input.GetAxis("Mouse X"), 0f), Space.World);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,19 +6,24 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     private Animator anim;
+    private HeadController hc;
     private float speed = 5f;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+        hc = GetComponentInChildren<HeadController>();
     }
 
     private void FixedUpdate()
     {
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
-        anim.SetBool("IsRunning", hor != 0f && ver != 0f);
-        rb.velocity = new Vector3(hor * speed, rb.velocity.y, ver * speed);
+        anim.SetBool("IsRunning", hor != 0f || ver != 0f);
+        Vector3 movement = hc.transform.forward * ver * speed;
+        movement += hc.transform.right * hor * speed;
+        movement.y = 0f;
+        rb.velocity = movement;
     }
 }
